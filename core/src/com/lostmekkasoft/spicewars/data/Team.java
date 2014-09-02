a465b1bcfc1aca1fbbc4b38c722c445393d3d872
Josephine Rehak <justallphine@googlemail.com>
2014-09-02 17:17:36 +0200

    implemented team class

--- core/src/com/lostmekkasoft/spicewars/data/Team.java
+++ core/src/com/lostmekkasoft/spicewars/data/Team.java
@@ -4,10 +4,26 @@
  */
 package com.lostmekkasoft.spicewars.data;
 
+import com.badlogic.gdx.graphics.Color;
+
 /**
  *
  * @author LostMekka
  */
 public class Team {
+	public int id;
+	public Color color;
+	public int maxStorage;
+	public int maxEnergy;
+	public float spiceStored = 0;
+	public float spiceIncome = 0;
+	public float energyStored = 0;
+	public float energyIncome = 0;
+	
+	
+	public Team(int id, Color color) {
+		this.id = id;
+		this.color = color;
+	}
 	
 }
