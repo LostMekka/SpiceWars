153033c8530b9ce60e7db959d135c0ca8c3a9f31
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-09-02 18:05:41 +0200

    Planets should now (as in later) be rendered as simple shapes

--- core/src/com/lostmekkasoft/spicewars/GameplayScreen.java
+++ core/src/com/lostmekkasoft/spicewars/GameplayScreen.java
@@ -4,7 +4,10 @@
  */
 package com.lostmekkasoft.spicewars;
 
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.lostmekkasoft.spicewars.data.Army;
 import com.lostmekkasoft.spicewars.data.Planet;
 import java.util.ArrayList;
@@ -31,7 +34,18 @@ public class GameplayScreen implements Screen {
 
 	@Override
 	public void render(float delta) {
+		Gdx.gl.glClearColor(0,0,0,1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
+		game.shapes.begin(ShapeRenderer.ShapeType.Filled);
+
+		// Render all planets as simple shapes
+		for (Planet planet : planets) {
+			game.shapes.setColor(planet.team.color);
+			game.shapes.circle(planet.position.x, planet.position.y, planet.radius);
+		}
+
+		game.shapes.end();
 	}
 
 	@Override
