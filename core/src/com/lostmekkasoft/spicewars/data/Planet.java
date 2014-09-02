a7b8cdc169bfdd0b73c128a12675cabbea5dbf3e
Josephine Rehak <justallphine@googlemail.com>
2014-09-02 17:59:12 +0200

    implemented planet class

--- core/src/com/lostmekkasoft/spicewars/data/Planet.java
+++ core/src/com/lostmekkasoft/spicewars/data/Planet.java
@@ -4,10 +4,24 @@
  */
 package com.lostmekkasoft.spicewars.data;
 
+import java.util.LinkedList;
+
 /**
  *
  * @author LostMekka
  */
 public class Planet {
+	public int radius;
+	public Team team;
+	public int maxNormalSlots;
+	public int maxMineSlots;
+	public LinkedList<Building> normalSlots;
+	public LinkedList<Building> mineSlots;
+	public double hp = 100;
+	public Point position;
+	
+	public static enum PlanetType{
+		normal,station
+	}
 	
 }
