461aadb702bdbd23ba43d23669ab702f819f9243
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-09-02 17:02:39 +0200

    Changed Desktop resolution to 1280x960

--- desktop/src/com/lostmekkasoft/spicewars/desktop/DesktopLauncher.java
+++ desktop/src/com/lostmekkasoft/spicewars/desktop/DesktopLauncher.java
@@ -5,8 +5,14 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.lostmekkasoft.spicewars.SpiceWars;
 
 public class DesktopLauncher {
+
 	public static void main (String[] arg) {
+
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		config.title = "SpiceWars";
+		config.width = 1280;
+		config.height = 960;
+
 		new LwjglApplication(new SpiceWars(), config);
 	}
 }
