25a452e4f07f36bf0540244b86376bb488b2016e
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-09-02 17:41:31 +0200

    Iplemented GameplayScreen, going past the Main Menu works now.

--- core/src/com/lostmekkasoft/spicewars/GameplayScreen.java
+++ core/src/com/lostmekkasoft/spicewars/GameplayScreen.java
@@ -4,21 +4,63 @@
  */
 package com.lostmekkasoft.spicewars;
 
+import com.badlogic.gdx.Screen;
 import com.lostmekkasoft.spicewars.data.Army;
 import com.lostmekkasoft.spicewars.data.Planet;
 import java.util.ArrayList;
 
 /**
- *
+ * SpiceWars - com.lostmekkasoft.spicewars
  * @author LostMekka
  */
-public class GameplayScreen {
+
+public class GameplayScreen implements Screen {
+
+	SpiceWars game;
 
 	public ArrayList<Planet> planets = new ArrayList<>();
 	public ArrayList<Army> armies = new ArrayList<>();
-	
+
+	public GameplayScreen(final SpiceWars game) {
+		this.game = game;
+	}
+
 	public void newLevel(){
 		
 	}
-		
+
+	@Override
+	public void render(float delta) {
+
+	}
+
+	@Override
+	public void resize(int width, int height) {
+
+	}
+
+	@Override
+	public void show() {
+
+	}
+
+	@Override
+	public void hide() {
+
+	}
+
+	@Override
+	public void pause() {
+
+	}
+
+	@Override
+	public void resume() {
+
+	}
+
+	@Override
+	public void dispose() {
+
+	}
 }
