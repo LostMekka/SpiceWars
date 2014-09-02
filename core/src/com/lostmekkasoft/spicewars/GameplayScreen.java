828ba8a996399d6cefb749526648348977161b52
LostMekka <LostMekka@LostMekka-M1.fritz.box>
2014-09-02 17:29:09 +0200

    moved game logic to gameplay screen

---
+++ core/src/com/lostmekkasoft/spicewars/GameplayScreen.java
@@ -0,0 +1,24 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.lostmekkasoft.spicewars;
+
+import com.lostmekkasoft.spicewars.data.Army;
+import com.lostmekkasoft.spicewars.data.Planet;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author LostMekka
+ */
+public class GameplayScreen {
+
+	public ArrayList<Planet> planets = new ArrayList<>();
+	public ArrayList<Army> armies = new ArrayList<>();
+	
+	public void newLevel(){
+		
+	}
+		
+}
