e0561e6744b39813e9b6ad5b7738d004e1d3d8a0
LostMekka <lostmekka@web.de>
2014-09-03 14:30:38 +0200

    removed obsolete fight method of army

--- core/src/com/lostmekkasoft/spicewars/data/Army.java
+++ core/src/com/lostmekkasoft/spicewars/data/Army.java
@@ -66,33 +66,6 @@ public class Army {
 		for(int i=0; i<s; i++) oldArmies[i].ships = newArmies[i];
 	}
 	
-	public void fight(Army a, double time){
-		double[] own = new double[4];
-		double[] enemy = new double[4];
-		for(int srcI=1; srcI<4; srcI++){
-			double dmg1 = Math.ceil(ships[srcI]) * dps[srcI] * time;
-			double dmg2 = Math.ceil(ships[srcI]) * dps[srcI] * time;
-			for(int trgtI : priorities[srcI]){
-				if(dmg1 * dpsMods[srcI][trgtI] > a.ships[trgtI]){
-					enemy[trgtI] = 0;
-					dmg1 -= a.ships[trgtI] / dpsMods[srcI][trgtI];
-				} else {
-					enemy[trgtI] = a.ships[trgtI] - dmg1 * dpsMods[srcI][trgtI];
-					dmg1 = 0;
-				}
-				if(dmg2 * dpsMods[srcI][trgtI] > ships[trgtI]){
-					own[trgtI] = 0;
-					dmg2 -= ships[trgtI] / dpsMods[srcI][trgtI];
-				} else {
-					own[trgtI] = ships[trgtI] - dmg2 * dpsMods[srcI][trgtI];
-					dmg2 = 0;
-				}
-			}
-		}
-		ships = own;
-		a.ships = enemy;
-	}
-	
 	public void bombardPlanet(Planet p, double time){
 		for(int i=1; i<4; i++) if(!p.team.isNeutral()){
 			p.damageRandomBuilding(Math.ceil(ships[i]) * dps[i] * time, accuracy[i]);
