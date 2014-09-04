28d74bf7f56234bcdfa6548053c2f4ef4abe9eac
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-09-04 16:17:07 +0200

    There's going to be a few new actors, moved them to a separate package.

--- core/src/com/lostmekkasoft/spicewars/actors/PlanetActor.java
+++ core/src/com/lostmekkasoft/spicewars/actors/PlanetActor.java
@@ -1,9 +1,12 @@
-package com.lostmekkasoft.spicewars;
+package com.lostmekkasoft.spicewars.actors;
 
 import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.lostmekkasoft.spicewars.data.Planet;
+
 
 /**
  * SpiceWars - com.lostmekkasoft.spicewars
@@ -18,12 +21,21 @@ public class PlanetActor extends Actor {
 	float textureFactor = 2f;
 
 	public Planet planet;
+	public boolean selected = false;
 
 	public PlanetActor(Planet planet, TextureRegion textureRegion, float actorX, float actorY) {
 		this.planet = planet;
 		this.textureRegion = textureRegion;
 		this.actorX = actorX;
 		this.actorY = actorY;
+
+		setBounds(actorX, actorY, planet.radius*2, planet.radius*2);
+		addListener(new InputListener() {
+			public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+				((PlanetActor)event.getTarget()).selected = true;
+				return true;
+			}
+		});
 	}
 
 	@Override
---
+++ core/src/com/lostmekkasoft/spicewars/actors/SelectionActor.java
@@ -0,0 +1,10 @@
+package com.lostmekkasoft.spicewars.actors;
+
+/**
+ * SpiceWars - com.lostmekkasoft.spicewars
+ * @author Kilian Koeltzsch
+ */
+
+public class SelectionActor {
+
+}
