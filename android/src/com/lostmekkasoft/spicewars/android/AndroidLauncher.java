8c90a76f0a30fe75a22dfeaf0db484254bf9a848
Kilian Koeltzsch <koeltzsch@gmail.com>
2014-09-07 16:28:51 +0200

    Theoretically prepared all needed files for building on Android.

---
+++ android/src/com/lostmekkasoft/spicewars/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.lostmekkasoft.spicewars.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.lostmekkasoft.spicewars.SpiceWars;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new SpiceWars(), config);
+	}
+}
